407f6774103de62827b8904587b3d0b95781471e
babywhale321 <107947235+babywhale321@users.noreply.github.com>
2022-08-03 23:39:06 -0400

    Create ssh-script.sh

---
+++ ssh-script.sh
@@ -0,0 +1,16 @@
+#Made By: BabyWhale
+
+#public ipv4 of ssh server
+echo "What is the public ipv4 address of the ssh server you are accessing? 22.22.22.22 = an example"
+read -r ipvar
+
+#port of ssh server
+echo "What is the port number of the ssh server you are accessing? 22 = an example"
+read -r portvar
+
+#user of ssh server
+echo "What will be the username of the ssh server you are accessing? user22 = an example"
+read -r uservar
+
+#Variables from user input is put into proper syntax for ssh
+ssh -p $portvar $uservar@$ipvar
