cad0c62ecb9c23eeec058896366313336dc42379
babywhale321 <107947235+babywhale321@users.noreply.github.com>
2022-12-27 00:00:14 -0500

    Update ssh-script.sh

--- ssh-script.sh
+++ ssh-script.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
-#Made By: BabyWhale
 
 #public ipv4 of ssh server
 echo "What is the public ipv4 address of the ssh server you are accessing? 22.22.22.22 = an example"
-read -r ipvar
+read ipvar
 
 #port of ssh server
 echo "What is the port number of the ssh server you are accessing? 22 = an example"
-read -r portvar
+read portvar
 
 #user of ssh server
 echo "What is the username of the ssh server you are accessing? user22 = an example"
-read -r uservar
+read uservar
 
-#Variables from user input is put into proper syntax for ssh
-ssh -p $portvar $uservar@$ipvar
+#saves script under user prompts if they want to
+echo "Would you like to save this script? (y/n)"
+read savescriptvar
+
+if [ "$savescriptvar" = "y" ] || [ "$savescriptvar" = "yes" ] || [ "$savescriptvar" = "Y" ] || [ "$savescriptvar" = "Yes" ]
+then
+    echo "What will this file be called? my-ssh-script.sh = an example"
+    read savescriptyesvar
+    echo "ssh -p $portvar $uservar@$ipvar" >> $savescriptyesvar
+else
+    ssh -p $portvar $uservar@$ipvar
+fi
