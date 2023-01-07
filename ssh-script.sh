a618c84e4cfb2983f9b201cfcf0b5213eaff124b
babywhale321 <107947235+babywhale321@users.noreply.github.com>
2023-01-07 15:15:04 -0500

    Update ssh-script.sh

--- ssh-script.sh
+++ ssh-script.sh
@@ -1,25 +1,23 @@
 #!/bin/sh
 
 #public ipv4 of ssh server
-echo "What is the public ipv4 address of the ssh server you are accessing? 22.22.22.22 = an example"
-read ipvar
+read -p "What is the public ipv4 address of the ssh server you are accessing? 22.22.22.22 = an example" ipvar
 
 #port of ssh server
-echo "What is the port number of the ssh server you are accessing? 22 = an example"
-read portvar
+read -p "What is the port number of the ssh server you are accessing? 22 = an example" portvar
 
 #user of ssh server
-echo "What is the username of the ssh server you are accessing? user22 = an example"
-read uservar
+read -p "What is the username of the ssh server you are accessing? user22 = an example" uservar
 
 #saves script under user prompts if they want to
-echo "Would you like to save this script? (y/n)"
-read savescriptvar
+read -p "Would you like to save this script? (y/n)" savescriptvar
 
-if [ "$savescriptvar" = "y" ] || [ "$savescriptvar" = "yes" ] || [ "$savescriptvar" = "Y" ] || [ "$savescriptvar" = "Yes" ]
+# Convert savescriptvar to lowercase
+savescriptvar=$(echo "$savescriptvar" | tr '[:upper:]' '[:lower:]')
+
+if [ "$savescriptvar" = "y" ] || [ "$savescriptvar" = "yes" ]
 then
-    echo "What will this file be called? my-ssh-script.sh = an example"
-    read savescriptyesvar
+    read -p "What will this file be called? my-ssh-script.sh = an example" savescriptyesvar
     echo "ssh -p $portvar $uservar@$ipvar" >> $savescriptyesvar
 else
     ssh -p $portvar $uservar@$ipvar
